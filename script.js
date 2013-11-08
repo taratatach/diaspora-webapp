bb8edac142be00d9309cb73720985566b141bea3
flaburgan <flaburgan@geexxx.fr>
2013-11-08 14:06:58 -0800

    Use a regex to validate handle. Fix validation by other browser than Firefox which didn't know String.contains method

--- script.js
+++ script.js
@@ -2,14 +2,15 @@
 document.getElementById('submitButton').addEventListener('click', redirect, false);
 
 function redirect() {
-  var handle = document.getElementById('podurl').value.split('@');
-  var username = handle[0], url = handle[1];
+  var handle = document.getElementById('podurl').value;
+  var handleregexp = new RegExp(/[A-Za-z0-9_]+@(([a-zA-Z0-9\-]*)\.)+([A-Za-z0-9\-]{2,})/);
   
-  if (url === undefined || !url.contains('.')) {
+  if (!handleregexp.test(handle)) {
     var error_message = document.getElementById('error');
     error_message.className = '';
     error_message.textContent = '"' + handle + '" is not a correct handle!';
   } else {
-    window.location = 'https://' + url + '/users/sign_in?user[username]=' + username;
+    var splitted = handle.split('@');
+    window.location = 'https://' + splitted[1] + '/users/sign_in?user[username]=' + splitted[0];
   }
 }
