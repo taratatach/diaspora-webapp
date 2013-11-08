8fc0ec679696ab4d480e03e9c8f8ffc0106684aa
flaburgan <flaburgan@geexxx.fr>
2013-11-08 12:01:26 -0800

    Add pre-filled username on diaspora sign in page

--- script.js
+++ script.js
@@ -2,14 +2,14 @@
 document.getElementById('submitButton').addEventListener('click', redirect, false);
 
 function redirect() {
-  var input = document.getElementById('podurl').value.split('@');
-  var handle = input[0], url = input[1];
+  var handle = document.getElementById('podurl').value.split('@');
+  var username = handle[0], url = handle[1];
   
   if (url === undefined || !url.contains('.')) {
     var error_message = document.getElementById('error');
     error_message.className = '';
-    error_message.textContent = input + ' is not a correct handle!';
+    error_message.textContent = '"' + handle + '" is not a correct handle!';
   } else {
-    window.location = 'https://' + url + '/users/sign_in';
+    window.location = 'https://' + url + '/users/sign_in?user[username]=' + username;
   }
 }
