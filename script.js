cf97c0c4c3f7d8ab0a7477cdd3bfd863ae353be3
flaburgan <flaburgan@geexxx.fr>
2013-11-08 15:17:45 -0800

    Add the manifest and some icons Add installation button

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 
 document.getElementById('submitButton').addEventListener('click', redirect, false);
+document.getElementById('installLink').addEventListener('click', install, false);
 
 function redirect() {
   var handle = document.getElementById('podurl').value;
@@ -13,4 +14,17 @@ function redirect() {
     var splitted = handle.split('@');
     window.location = 'https://' + splitted[1] + '/users/sign_in?user[username]=' + splitted[0];
   }
+}
+
+function install() {
+  var request = window.navigator.mozApps.install("http://flaburgan.github.io/diaspora-webapp/manifest.webapp");
+  request.onsuccess = function () {
+    // Save the App object that is returned
+    var appRecord = this.result;
+    alert('Installation successful!');
+  };
+  request.onerror = function () {
+    // Display the error information from the DOMError object
+    alert('Install failed, error: ' + this.error.name);
+  };
 }
