68be256cfc753e822b68e2a175860a6aea5e992f
Flaburgan <flaburgan@geexxx.fr>
2014-11-18 15:42:34 +0100

    Add l10n in share script

--- script_share.js
+++ script_share.js
@@ -4,37 +4,35 @@ navigator.mozSetMessageHandler("activity", function(activity){
 		// obtain the URL from the activity
 		var urlToShare = activity.source.data.url;
 		if (window.localStorage.length > 1) {
-		var div = document.getElementById('previousHandles');
-		var existingHandlesLabel = document.createElement('p');
-		existingHandlesLabel.id = 'existing-handles-label';
-		existingHandlesLabel.appendChild(document.createTextNode('Please select one of your diaspora* ID:'));
-		div.appendChild(existingHandlesLabel);
+		  var div = document.getElementById('previousHandles');
+		  var existingHandlesLabel = document.createElement('p');
+		  existingHandlesLabel.id = 'existing-handles-label';
+		  existingHandlesLabel.setAttribute('data-l10n-id', 'select-id');
+		  div.appendChild(existingHandlesLabel);
 		
-		// Add handles to a list
-		var ul = document.createElement('ul');
-		for (var i = 0; i < window.localStorage.length; i++) {
-			var handle = window.localStorage.key(i);
-			var li = document.createElement('li');
-			var linkToPod = document.createElement('a');
-		 
-			linkToPod.setAttribute('href', getShareUrl(handle, urlToShare));
-			linkToPod.setAttribute('title', 'Share to my pod!');
-			linkToPod.appendChild(document.createTextNode(handle));
-		 
-			li.appendChild(linkToPod);
-			ul.appendChild(li);
-		}
-		div.appendChild(ul);
-		}
-		else if (window.localStorage.length == 1){
+		  // Add handles to a list
+		  var ul = document.createElement('ul');
+		  for (var i = 0; i < window.localStorage.length; i++) {
+			  var handle = window.localStorage.key(i);
+			  var li = document.createElement('li');
+			  var linkToPod = document.createElement('a');
+		   
+			  linkToPod.setAttribute('href', getShareUrl(handle, urlToShare));
+			  linkToPod.setAttribute('title', 'Share to my pod!');
+			  linkToPod.appendChild(document.createTextNode(handle));
+		   
+			  li.appendChild(linkToPod);
+			  ul.appendChild(li);
+		  }
+		  div.appendChild(ul);
+		} else if (window.localStorage.length == 1){
 				//if only one account, share directly to it
-				var handle = window.localStorage.key(0);
-				
+				var handle = window.localStorage.key(0);				
 				window.location = getShareUrl(handle, urlToShare);
-		}
-		else{
-				// say that config is needed
-				alert('Please configure a Diaspora* account first.');
+		} else{
+				// If no diaspora* account has been defined.
+        //TODO: propose for configuration and continue
+        alert(navigator.mozL10n.get('configuration-required'));
 		}
 	}
 });
